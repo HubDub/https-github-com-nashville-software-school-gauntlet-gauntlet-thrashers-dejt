59c9cd2a0db6e86e496a00a127d9a0c02fc734b1
Thomas J. Shavor <thomasshavor@gmail.com>
2016-08-11 10:55:46 -0500

    final html changes made

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -29,54 +29,68 @@ var WarAxe = function() {
 };
 WarAxe.prototype = new Weapon();
 
-// var Javelin = function() {
-//   this.name = "Javelin";
-//   this.damage = 10;
-//   this.hands = 1;
-// };
-// Javelin.prototype = new Weapon();
+var Javelin = function() {
+  this.name = "Javelin";
+  this.damage = 10;
+  this.hands = 1;
+};
+Javelin.prototype = new Weapon();
 
-// var ShortBow = function() {
-//   this.name = "ShortBow";
-//   this.damage = 8;
-//   this.hands = 2;
-// };
-// ShortBow.prototype = new Weapon();
+var ShortBow = function() {
+  this.name = "ShortBow";
+  this.damage = 8;
+  this.hands = 2;
+};
+ShortBow.prototype = new Weapon();
 
-// var Glaive = function() {
-//   this.name = "Glaive";
-//   this.damage = 15;
-//   this.hands = 2;
-// };
-// Glaive.prototype = new Weapon();
+var Glaive = function() {
+  this.name = "Glaive";
+  this.damage = 15;
+  this.hands = 2;
+};
+Glaive.prototype = new Weapon();
 
-// var Warhammer = function() {
-//   this.name = "Warhammer";
-//   this.damage = 22;
-//   this.hands = 2;
-// };
-// Warhammer.prototype = new Weapon();
+var Warhammer = function() {
+  this.name = "Warhammer";
+  this.damage = 22;
+  this.hands = 2;
+};
+Warhammer.prototype = new Weapon();
 
-// var Trident = function() {
-//   this.name = "Trident";
-//   this.damage = 20;
-//   this.hands = 2;
-// };
-// Trident.prototype = new Weapon();
+var Trident = function() {
+  this.name = "Trident";
+  this.damage = 20;
+  this.hands = 2;
+};
+Trident.prototype = new Weapon();
 
-// var Blowgun = function() {
-//   this.name = "Blowgun";
-//   this.damage = 5;
-//   this.hands = 2;
-// };
-// Blowgun.prototype = new Weapon();
+var Blowgun = function() {
+  this.name = "Blowgun";
+  this.damage = 5;
+  this.hands = 2;
+};
+Blowgun.prototype = new Weapon();
 
-// var ThrowingStar = function() {
-//   this.name = "ThrowingStar";
-//   this.damage = 18;
-//   this.hands = 2;
-// };
-// ThrowingStar.prototype = new Weapon();
+var ThrowingStar = function() {
+  this.name = "ThrowingStar";
+  this.damage = 18;
+  this.hands = 2;
+};
+ThrowingStar.prototype = new Weapon();
+
+var Flail = function() {
+  this.name = "Flail";
+  this.damage = 18;
+  this.hands = 2;
+};
+Flail.prototype = new Weapon();
+
+var Whip = function() {
+  this.name = "Whip";
+  this.damage = 18;
+  this.hands = 2;
+};
+Whip.prototype = new Weapon();
 
 
 
